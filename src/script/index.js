2a75d74010e6e791bf4e08b907f9b64e676a0b25
SparkChain138 <hiroakiho0@gmail.com>
2022-11-20 21:30:10 +0900

    other js file is called by index.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
 // index.html内で読み込むcssをimport
 import "../styles/style.scss";
 import "../styles/reset.scss";
+import { initializeLeafletMap } from "./leafletMap";
 
 // index.html内で実行するJavaScript
 const hello = "hello world";
 console.log(hello);
+initializeLeafletMap();
--- src/script/leafletMap.js
+++ src/script/leafletMap.js
@@ -1,12 +1,15 @@
-console.log("hoge")
+export const initializeLeafletMap = () => {
+  console.log("hoge")
 
-const map = L.map('map').setView([35.604399, 139.683831], 30);
+  const map = L.map('map').setView([35.604399, 139.683831], 18);
 
-L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-}).addTo(map);
+  L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
+  }).addTo(map);
 
-//マーカー表示の記述
-L.marker([35.604399, 139.683831]).addTo(map)
-  .bindPopup('ここにいます')
-  .openPopup();
+  //マーカー表示の記述
+  L.marker([35.604399, 139.683831]).addTo(map)
+    .bindPopup('ここにいます')
+    .openPopup();
+}
